30d1c854ebf4cf2caf0fa35844e379ecb7267b8a
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-06-24 23:04:23 +0530

    Elements

---
+++ Elements.java
@@ -0,0 +1,27 @@
+public class Elements{
+	public static void main(String[] args){
+	int[] primeNumbers ={1,2,7,11,13};
+	String[] name={"swati","neelu","rashmi","seema","shantala","farjana"};
+	char[] grades={'A','B','C','D'};
+	short[] tables={10,20,30,40,50};
+	byte[] singleDigits={1,2,3,4,5,6};
+	float[] weight={61.0f,60.7f,78.9f};
+	double[] height={167.55,179.90,156.98,167.78};
+	boolean[] courtDecision={true,false,true,false};
+	long[] largeNumbers={2170000L,87653000L};
+	
+	System.out.println("prime Numbers:"+primeNumbers+" length: "+primeNumbers.length);
+	System.out.println("Names:"+name+" length: "+name.length);
+	System.out.println("grades:"+grades+" length: "+grades.length);
+	System.out.println("tables:"+tables+" length: "+tables.length);
+	System.out.println("singleDigits:"+singleDigits+" length: "+singleDigits.length);
+	System.out.println("weight:"+weight+" length: "+weight.length);
+	System.out.println("height:"+height+" length: "+height.length);
+	System.out.println("courtDecision:"+courtDecision+" length: "+courtDecision.length);
+	System.out.println("largeNumbers:"+largeNumbers+" length: "+largeNumbers.length);
+	}
+
+
+
+
+}
