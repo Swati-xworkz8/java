37aaff729ae0fef9d376608ba22147145500a9db
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-06-24 12:54:17 +0530

    this is my first commit

---
+++ Capsule.java
@@ -0,0 +1,11 @@
+class Capsule{
+	public static void main (String [] args){
+	System.out.println("NAME OF THE TABLET:"+Tablet.name);
+	System.out.println("BRAND NAME:"+Tablet.brandName);
+	System.out.println("TOTAL NO OF PACKETS WE HAVE:"+Tablet.packets);
+	System.out.println("PRICE PER TABLET:"+Tablet.price);
+	System.out.println("CONTENT OF PARACETAMAL IN DOLO 650:"+Tablet.contentOfParacetamol);
+	}
+
+
+}
