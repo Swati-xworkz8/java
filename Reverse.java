17c74f5de1aff382ab5af667e1d35b30d72bc51c
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-06-26 10:41:28 +0530

    Reverse order 26/6

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse{
+	public static void main(String[] args){
+	int []wholeNumbers={2,9,7,4,6,5};
+    for (int index=wholeNumbers.length-1;index>=0;index--){
+	System.out.print(" "+wholeNumbers[index]);
+	
+	}
+	}
+
+
+}
