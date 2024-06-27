4a16f8eaa69d83095c7abd5055397e10e39b86db
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-06-27 10:15:17 +0530

    multiple of 3: 27/6/24

---
+++ Multiple.java
@@ -0,0 +1,24 @@
+public class Multiple{
+	public static void main(String [] args){
+	int []threeMultiples={3,6,9,12,15,18,21,24,27,30,97};
+	
+	for(int i=0;i<threeMultiples.length;i++){
+	if(threeMultiples[i] % 3 == 0){
+		System.out.println(" "+threeMultiples[i]);
+	}else{
+	System.out.println("not a 3 multples:"+threeMultiples[i]);
+	
+	}
+	}
+	
+	
+	}
+
+
+
+
+}
+
+
+
+
