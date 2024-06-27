24f594c28a08813c80113a3d63130153530118b0
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-06-27 10:12:18 +0530

    sum 0f number: 27/6/24

---
+++ Sum1.java
@@ -0,0 +1,15 @@
+public class Sum1{
+	public static void main(String[] args){
+	int[] squareNumbers={1,2,4,9,16,25,36,49};
+	int sum = 0;
+	for(int index=0;index<squareNumbers.length;index++){
+		sum = sum+squareNumbers[index];
+	    System.out.println("sum of squareNumbers are:"+sum);
+	}
+	
+	
+	}
+
+
+
+}
