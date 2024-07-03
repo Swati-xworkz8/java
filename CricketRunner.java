073d36a70bec0c316f319246ee5fd073f0b8ae77
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-07-03 09:57:19 +0530

    methods 3/7/2024

---
+++ CricketRunner.java
@@ -0,0 +1,9 @@
+public class CricketRunner{
+	public static void main(String []args){
+	Cricket.runRate(178,"Virat kohli");
+	Cricket.match(189,237);
+	Cricket.player(11);
+	Cricket.playerDetails("Swati",22,166.7f,61.4,"B.E",43,237);
+	}
+
+}
