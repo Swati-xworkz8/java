5037af6f79b983c142d3051aee7181cbb0f7f03b
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-07-03 09:46:58 +0530

    method

---
+++ Person.java
@@ -0,0 +1,19 @@
+public class Person{
+	public static void details(int age){
+	System.out.println("person's age:"+age);
+	}
+	public static void details(int age,String name){
+	System.out.println("person age:"+age+" "+"name:"+name);
+	}
+	public static void details(int age,String name,long salary){
+	System.out.println("person age:"+age+" "+"name:"+name+" "+"salary:"+salary);
+	}
+	public static void details(int age,String name,char bloodGroup,long salary,double weight){
+	System.out.println("person age:"+age+" "+"name:"+name+" "+"bloodGroup:"+bloodGroup+" "+"salary:"+salary+"weight:"+weight);
+	}
+	public static void details(int age,String name,char bloodGroup,long salary,double weight,float height){
+		System.out.println("person age:"+age+" "+"name:"+name+" "+"bloodGroup:"+bloodGroup+" "+"salary:"+salary+"weight:"+weight+"height:"+height);
+
+	}
+
+}
