b7eb6f7a3a9530454c39e54444154c95202439ab
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-07-03 09:48:39 +0530

    Method

---
+++ ShoppingMall.java
@@ -0,0 +1,14 @@
+public class ShoppingMall{
+	public static void shoppingType(String shoppingType){
+	System.out.println("type of shopping:"+shoppingType);
+	}
+	public static void gstPrice(int price){
+	float totalPrice=price+(price*0.18f);
+	System.out.println("total price:"+totalPrice);
+	}
+	public static void itemName(String name){
+	System.out.println("item name:"+name);
+	}
+	
+
+}
