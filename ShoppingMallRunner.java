e8f488171c0d1e03b9ff9efbd4648880294c5105
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-07-03 09:52:23 +0530

    methods

---
+++ ShoppingMallRunner.java
@@ -0,0 +1,9 @@
+public class ShoppingMallRunner{
+	public static void main(String []args){
+	ShoppingMall.shoppingType("Accessories");
+	ShoppingMall.gstPrice(1000);
+	ShoppingMall.itemName("bag");
+	
+	}
+
+}
