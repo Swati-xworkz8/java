6a9f6eb99730e46574ab7eeb595c35eb89c43c3f
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-07-03 10:31:20 +0530

    methods

---
+++ Metroo.java
@@ -0,0 +1,16 @@
+public class Metroo{
+	public static void transpotation(String from,String to){
+	//System.out.println("metro starts from:"+from);
+	//System.out.println("metro ends at:"to);
+	if(from=="banashankari" && to=="btm"){
+		System.out.println("price of the ticket is 25"+from+" "+to);
+	}else{
+	System.out.println("price is 50rs");
+	}
+	//System.out.println("price:"+price);
+	
+	
+	}
+
+
+}
