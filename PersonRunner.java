bd232cbb76ee4df8c4141c2a25d4d120963b6c3d
Rayara Anugraha <swatig.xworkz@gmail.com>
2024-07-03 09:53:22 +0530

    methods

---
+++ PersonRunner.java
@@ -0,0 +1,13 @@
+public class PersonRunner{
+	public static void main(String []args){
+	Person.details(22);
+	Person.details(22,"swati");
+	Person.details(22,"swati",3500009L);
+	Person.details(22,"swati",'o',3565457L,61.91);
+	Person.details(22,"swati",'o',3456789L,61.91,167.8f);
+	
+	
+	
+	}
+
+}
